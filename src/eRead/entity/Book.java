5bf4881e8e4a5b520e962079addc5a9a918fbbaf
GitHub-Anish <110275494+GitHub-Anish@users.noreply.github.com>
2022-07-30 01:33:03 -0400

    Add files via upload

--- src/eRead/entity/Book.java
+++ src/eRead/entity/Book.java
@@ -1,1 +1,74 @@
 
+package eRead.entity;
+
+
+public class Book {
+    
+    private String bookName , bookId ,author ,stock,status ;
+
+    /**
+     * @return the bookName
+     */
+    public String getBookName() {
+        return bookName;
+    }
+
+    /**
+     * @param bookName the bookName to set
+     */
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    /**
+     * @return the bookId
+     */
+    public String getBookId() {
+        return bookId;
+    }
+
+    /**
+     * @param bookId the bookId to set
+     */
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the stock
+     */
+    public String getStock() {
+        return stock;
+    }
+    
+
+    /**
+     * @param stock the stock to set
+     */
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+               
+    
+    
+}
---
+++ src/eRead/entity/User.java
@@ -0,0 +1,65 @@
+
+package eRead.entity;
+
+
+public class User {
+  
+        private String userId,password,role,name;
+
+    /**
+     * @return the userId
+     */
+    public String getUserId() {
+        return userId;
+    }
+
+    /**
+     * @param userId the userId to set
+     */
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the role
+     */
+    public String getRole() {
+        return role;
+    }
+
+    /**
+     * @param role the role to set
+     */
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+}
